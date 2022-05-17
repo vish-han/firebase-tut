66037251a7d6d3cd52c60cc0434396ed34b6f10d
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-05-17 12:37:50 +0530

    crud operation completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {collection,getDocs} from 'firebase/firestore'
+import {collection,getDocs,addDoc,updateDoc,doc,deleteDoc} from 'firebase/firestore'
 import {db} from './firebase-config'
 import {useEffect,useState} from 'react';
 function App() {
@@ -10,23 +10,33 @@ function App() {
   useEffect(()=>{
  const getuser=( async ()=>{
       const data=await getDocs(userCollectionReference)
-      console.log(data.docs);
 
       setUsers(data.docs.map((doc)=>({...doc.data(),id:doc.id})))
+      console.log(users);
       })
     getuser();
   },[])
   const createUser=async ()=>{
-     
+     await addDoc(userCollectionReference,{name:newName,age:Number(newAge)})
+  }
+  const updateAge=async (id ,age)=>{
+const newFeild={age:age+1};
+const userdoc=doc(db,"users",id);
+await updateDoc(userdoc,newFeild);
+  }
+  const deleteUser=async (id)=>{
+   const user=doc(db,"users",id);
+   await deleteDoc(user);
   }
   return (
     <div className="App">
-      <input placeholder="name" onchange={e=>{setNewName(e.target.value)}}></input>
-      <input placeholder="age" onchange={e=>{setNewAge(e.target.value)}}/>
+      <input placeholder="name" onChange={(event)=>{setNewName(event.target.value)}}/>
+      <input placeholder="age" type="number" onChange={(event)=>{setNewAge(event.target.value)}}/>
       <button onClick={createUser}>Create User</button>
     {users.map((user)=>{return<> <h1>NAME: {user.name}</h1>
     <h1>AGE :{user.age}</h1>
-    <h1>ID :{user.id}</h1>
+   <button onClick={()=>updateAge(user.id,user.age)}>Increment Age</button>
+   <button onClick={()=>deleteUser(user.id)}>Delete User</button>
     </>})}
     </div>
   );
