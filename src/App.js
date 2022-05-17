21a64cb0b66828376f45fc0ba684f84833607443
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-05-17 12:01:42 +0530

    input and button created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {db} from './firebase-config'
 import {useEffect,useState} from 'react';
 function App() {
   const [users,setUsers]=useState([]);
+  const [newName,setNewName]=useState("");
+  const [newAge,setNewAge]=useState(0);
   const userCollectionReference=collection(db,"users")
   useEffect(()=>{
  const getuser=( async ()=>{
@@ -14,8 +16,14 @@ function App() {
       })
     getuser();
   },[])
+  const createUser=async ()=>{
+     
+  }
   return (
     <div className="App">
+      <input placeholder="name" onchange={e=>{setNewName(e.target.value)}}></input>
+      <input placeholder="age" onchange={e=>{setNewAge(e.target.value)}}/>
+      <button onClick={createUser}>Create User</button>
     {users.map((user)=>{return<> <h1>NAME: {user.name}</h1>
     <h1>AGE :{user.age}</h1>
     <h1>ID :{user.id}</h1>
